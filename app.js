37b61b61e75d1a9dfe0b1d138265bc04ef6f76e7
Huzaifa Malick <135388170+Huzaifamalic@users.noreply.github.com>
2023-10-22 19:32:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,70 @@
+// Q --1 ----TO 3
+
+
+
+let num1 = +prompt("Enter Your add Number 1");
+let num2 = +prompt("Enter Your add Number 2");
+let add = num1 + num2 ;
+document.write(
+    "<table border= '1px'  >"+
+    "<tr>"+
+    "<th>"+"Addititon"+"</th>"
+    +"</tr>"+
+    "<tr>"+
+    "<td>"+ add +"</td>"
+    +"</tr>"
+
+    +"</table>"
+);
+
+    let num3= +prompt("Enter Your subtrac Number 1");
+let num4 = +prompt("Enter Your subtrac Number 2");
+let add2 = num3 - num4 ;
+
+document.write(
+    "<table border= '1px'  >"+
+    "<tr>"+
+    "<th>"+"subtract"+"</th>"
+    +"</tr>"+
+    "<tr>"+
+    "<td>"+ add2 +"</td>"
+    +"</tr>"
+
+    +"</table>"
+);
+
+
+let num5= +prompt("Enter Your divide Number 1");
+let num6 = +prompt("Enter Your divide Number 2");
+let add3 = num5 / num6 ;
+document.write(
+    "<table border= '1px' >"+
+    "<tr>"+
+    "<th>"+"division"+"</th>"
+    +"</tr>"+
+    "<tr>"+
+    "<td>"+ add3 +"</td>"
+    +"</tr>"
+
+    +"</table>"
+);
+let num7= +prompt("Enter YourMultiplai Number 1");
+let num8 = +prompt("Enter YourMultiplai Number 2");
+let add4 = num7 * num8 ;
+document.write(
+    "<table border= '1px' >"+
+    "<tr>"+
+    "<th>"+"Multiplai"+"</th>"
+    +"</tr>"+
+    "<tr>"+
+    "<td>"+ add4 +"</td>"
+    +"</tr>"
+
+    +"</table>"
+)
+
+
+
+
+ 
+
